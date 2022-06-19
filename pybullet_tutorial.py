66fcd637b1067b35af3ccc745f7d82a658e7e30b
Abhishek Kashyap <abhishek47kashyap@gmail.com>
2022-06-19 14:45:28 +0000

    get camera link state

--- pybullet_tutorial.py
+++ pybullet_tutorial.py
@@ -4,33 +4,32 @@ Ref:
 """
 
 import time
+from copy import deepcopy
 
 import numpy as np
 import trimesh
 
 import pybullet as p
-import pybulletX as px
 import pybullet_data
-import pybullet_utils.bullet_client as bc
 
-CAMERA_XTION_CONFIG = [{
+CAMERA_XTION_CONFIG = {
     'image_size': (480, 640),
     'intrinsics': (537.4933389299223, 0.0, 319.9746375212718, 0.0, 536.5961755975517, 244.54846607953, 0.0, 0.0, 1.0),
     'position': None,
-    'rotation': None,
+    'orientation': None,
     'zrange': (0.5, 10.),
     'noise': False
-}]
+}
 
-CAMERA_REALSENSE_CONFIG = [{
+CAMERA_REALSENSE_CONFIG = {
     'image_size': (480, 640),
     'intrinsics': (
     607.3814086914062, 0.0, 315.9123840332031, 0.0, 607.2514038085938, 233.77308654785156, 0.0, 0.0, 1.0),
     'position': None,
-    'rotation': None,
+    'orientation': None,
     'zrange': (0.3, 10.),
     'noise': False
-}]
+}
 
 hand = True   # if false, CAMERA_XTION_CONFIG config will be used
 
@@ -39,16 +38,10 @@ def main():
     use_fixed_base = True
     connection_mode = p.GUI
 
-    pybullet_client_gui = p.connect(connection_mode)
-    # bullet_client_gui = bc.BulletClient(connection_mode=connection_mode)
+    camera_joint_frame = 'hand_camera_gazebo_frame_joint' if hand else 'head_rgbd_sensor_gazebo_frame_joint'
+    camera_config = deepcopy(CAMERA_REALSENSE_CONFIG if hand else CAMERA_XTION_CONFIG)
 
-    # px_gui = px.Client(client_id=bullet_client_gui._client)
-
-    # robot = px.Robot(
-    #     urdf_file_path,
-    #     use_fixed_base=use_fixed_base,
-    #     physics_client=px_gui
-    #     )
+    pybullet_client = p.connect(connection_mode)
 
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
     p.loadURDF("plane.urdf")
@@ -64,6 +57,7 @@ def main():
     joint_indices = range(n_joints)
 
     print("Joint information:")
+    camera_link_idx = None
     for joint in joints:
         idx = joint[0]
         name = joint[1]
@@ -72,6 +66,12 @@ def main():
         upper_limit = joint[9]
         link_name = joint[12]
         print(f"  Joint {idx}: {name}, type: {joint_type}, limits: lower {lower_limit}, upper: {upper_limit}, link-name: {link_name}")
+
+        if name.decode('ASCII') == camera_joint_frame:   # decode() required because name is a byte-string
+            camera_link_idx = idx
+            print(f"Camera link name is {camera_joint_frame}, link index is {camera_link_idx}")
+    if not camera_link_idx:
+        print(f"Camera link index not found for frame named {camera_joint_frame}")
 
     # The magic that enables torque control
     p.setJointMotorControlArray(
@@ -117,8 +117,6 @@ def main():
     p.changeDynamics(obj_id, -1, lateralFriction=0.25)
     p.setGravity(0, 0, -9.8)
 
-
-
     width = 128
     height = 128
 
@@ -134,7 +132,31 @@ def main():
     projection_matrix = p.computeProjectionMatrixFOV(fov, aspect, near, far)
     renderer = p.ER_BULLET_HARDWARE_OPENGL   # or p.ER_TINY_RENDERER
 
+    first_iteration = True
+
     while True:
+        camera_link_state = p.getLinkState(
+            robot_body_unique_id,
+            linkIndex=camera_link_idx,
+            computeForwardKinematics=1,  # True
+            physicsClientId=pybullet_client
+        )
+        camera_link_position = camera_link_state[0]      # tuple of 3 floats, same values as index [4]
+        camera_link_orientation = camera_link_state[1]   # tuple of 4 floats, same values as index [5]
+
+        camera_config["position"] = list(camera_link_position)
+        camera_config["orientation"] = list(camera_link_orientation)
+
+        if first_iteration:
+            first_iteration = False
+            print(f"{camera_joint_frame} info:")
+            for elem in camera_link_state:
+                print(f"\t{elem}")
+
+            print("Camera config:")
+            print(camera_config)
+
+
         images = p.getCameraImage(width, height, view_matrix, projection_matrix, renderer=renderer)
         depth_buffer_opengl = np.reshape(images[3], [width, height])
         depth_opengl = far * near / (far - (far - near) * depth_buffer_opengl)
