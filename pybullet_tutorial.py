35c7c072c828b9344b29a084a2c5d5b0b6998083
Abhishek Kashyap <abhishek47kashyap@gmail.com>
2022-07-04 22:24:51 +0900

    introduces pybulletX to get joint values
    didn't manage to figure out why pybullet's getJointStates() always returns only zeros

--- pybullet_tutorial.py
+++ pybullet_tutorial.py
@@ -5,6 +5,7 @@ Ref:
 
 import time
 from copy import deepcopy
+from enum import Enum
 
 import numpy as np
 from scipy.spatial.transform import Rotation as R
@@ -12,6 +13,63 @@ import trimesh
 
 import pybullet as p
 import pybullet_data
+import pybullet_utils.bullet_client as bc
+import pybulletX as px
+
+all_joint_names = [  # order is important
+    'joint_x',
+    'joint_y',
+    'joint_rz',
+    'torso_lift_joint',
+    'head_pan_joint',
+    'head_tilt_joint',
+    'arm_lift_joint',
+    'arm_flex_joint',
+    'arm_roll_joint',
+    'wrist_flex_joint',
+    'wrist_roll_joint',
+    'hand_l_proximal_joint',
+    'hand_l_distal_joint',
+    'hand_r_proximal_joint',
+    'hand_r_distal_joint'
+    ]
+
+class JointName(Enum):
+    joint_x = all_joint_names[0]
+    joint_y = all_joint_names[1]
+    joint_rz = all_joint_names[2]
+    torso_lift_joint = all_joint_names[3]
+    head_pan_joint = all_joint_names[4]
+    head_tilt_joint = all_joint_names[5]
+    arm_lift_joint = all_joint_names[6]
+    arm_flex_joint = all_joint_names[7]
+    arm_roll_joint = all_joint_names[8]
+    wrist_flex_joint = all_joint_names[9]
+    wrist_roll_joint = all_joint_names[10]
+    hand_l_proximal_joint = all_joint_names[11]
+    hand_l_distal_joint = all_joint_names[12]
+    hand_r_proximal_joint = all_joint_names[13]
+    hand_r_distal_joint = all_joint_names[14]
+
+
+def get_joint_value(robot_body_unique_id, joint_name: JointName):
+    if joint_name.value not in all_joint_names:
+        raise ValueError(f"Joint name {str(joint_name)} is invalid")
+    
+    joint_values = robot_body_unique_id.get_states().joint_position
+
+    return joint_values[all_joint_names.index(joint_name.value)]
+
+def get_all_joint_values(robot_body_unique_id, verbose=False):
+    joint_values = robot_body_unique_id.get_states().joint_position
+
+    if verbose:
+        print("All joint values:")
+        for name, value in zip(all_joint_names, joint_values):
+            print(f"\t{name}: {value}")
+
+    return joint_values
+
 
 CAMERA_XTION_CONFIG = {
     'image_size': (480, 640),
@@ -34,20 +92,20 @@ CAMERA_REALSENSE_CONFIG = {
 
 hand = False   # if false, CAMERA_XTION_CONFIG config will be used
 
-def get_view_matrix(camera_config: dict):
+def get_view_matrix(pybullet_client, camera_config: dict):
     # OpenGL camera settings.
     lookdir = np.float32([0, 0, 1]).reshape(3, 1)
     updir = np.float32([0, -1, 0]).reshape(3, 1)
 
-    rotm = p.getMatrixFromQuaternion(camera_config['orientation'])
+    rotm = pybullet_client.getMatrixFromQuaternion(camera_config['orientation'])
     rotm = np.float32(rotm).reshape(3, 3)
     lookdir = (rotm @ lookdir).reshape(-1)
     lookat = camera_config['position'] + lookdir
     updir = (rotm @ updir).reshape(-1)
 
-    return p.computeViewMatrix(camera_config['position'], lookat, updir)
+    return pybullet_client.computeViewMatrix(camera_config['position'], lookat, updir)
 
-def get_projection_matrix(camera_config: dict):
+def get_projection_matrix(pybullet_client, camera_config: dict):
     focal_len = camera_config['intrinsics'][0]
     fovh = (camera_config['image_size'][0] / 2) / focal_len
     fovh = 180 * np.arctan(fovh) * 2 / np.pi
@@ -56,7 +114,7 @@ def get_projection_matrix(camera_config: dict):
     znear, zfar = camera_config['zrange']
     aspect_ratio = camera_config['image_size'][1] / camera_config['image_size'][0]
 
-    return p.computeProjectionMatrixFOV(fovh, aspect_ratio, znear, zfar)
+    return pybullet_client.computeProjectionMatrixFOV(fovh, aspect_ratio, znear, zfar)
 
 def main():
     urdf_file_path = "hsrb_description/robots/hsrb.urdf"
@@ -66,50 +124,15 @@ def main():
     camera_joint_frame = 'hand_camera_gazebo_frame_joint' if hand else 'head_rgbd_sensor_gazebo_frame_joint'
     camera_config = deepcopy(CAMERA_REALSENSE_CONFIG if hand else CAMERA_XTION_CONFIG)
 
-    pybullet_client = p.connect(connection_mode)
+    bullet_client = bc.BulletClient(connection_mode=connection_mode)
+    px_client = px.Client(client_id=bullet_client._client)
 
-    p.setAdditionalSearchPath(pybullet_data.getDataPath())
-    p.loadURDF("plane.urdf")
+    bullet_client.setAdditionalSearchPath(pybullet_data.getDataPath())
+    bullet_client.loadURDF("plane.urdf")
 
-    robot_body_unique_id = p.loadURDF(
-        urdf_file_path,
-        useFixedBase=use_fixed_base,
-        flags=p.URDF_USE_SELF_COLLISION
-        )
+    robot_body_unique_id = px.Robot(urdf_file_path, use_fixed_base=use_fixed_base, physics_client=px_client, base_position=[0.0, 0.0, 0.0])
+    robot_body_unique_id.torque_control = True
 
-    n_joints = p.getNumJoints(robot_body_unique_id)
-    joints = [p.getJointInfo(robot_body_unique_id, i) for i in range(n_joints)]
-    joint_indices = range(n_joints)
-
-    print("Joint information:")
-    camera_link_idx = None
-    for joint in joints:
-        idx = joint[0]
-        name = joint[1]
-        joint_type = joint[2]
-        lower_limit = joint[8]
-        upper_limit = joint[9]
-        link_name = joint[12]
-        print(f"  Joint {idx}: {name}, type: {joint_type}, limits: lower {lower_limit}, upper: {upper_limit}, link-name: {link_name}")
-
-        if name.decode('ASCII') == camera_joint_frame:   # decode() required because name is a byte-string
-            camera_link_idx = idx
-            print(f"Camera link name is {camera_joint_frame}, link index is {camera_link_idx}")
-    if not camera_link_idx:
-        print(f"Camera link index not found for frame named {camera_joint_frame}")
-
-    joint_states = p.getJointStates(robot_body_unique_id, range(n_joints), pybullet_client)
-    print("\nJoint values as obtained from p.getJointStates():")
-    print(joint_states)
-    print()
-
-    # The magic that enables torque control
-    p.setJointMotorControlArray(
-        bodyIndex=robot_body_unique_id,
-        jointIndices=joint_indices,
-        controlMode=p.TORQUE_CONTROL,
-        forces=np.zeros(n_joints),
-    )
 
     model_name = "007_tuna_fish_can"
     mesh_path = 'assets/ycb/{}/google_16k/nontextured.stl'.format(model_name)
@@ -122,20 +145,20 @@ def main():
     scale = 1
     scale = [scale, scale, scale]
 
-    visual_shape_id = p.createVisualShape(
-        shapeType=p.GEOM_MESH,
+    visual_shape_id = bullet_client.createVisualShape(
+        shapeType=bullet_client.GEOM_MESH,
         rgbaColor=[1, 0, 0, 1],
         specularColor=[0.4, .4, 0],
         fileName=mesh_path, meshScale=scale,
         visualFramePosition=-centroid,
     )
-    collision_shape_id = p.createCollisionShape(
-        shapeType=p.GEOM_MESH,
+    collision_shape_id = bullet_client.createCollisionShape(
+        shapeType=bullet_client.GEOM_MESH,
         fileName=collision_path, meshScale=scale,
         collisionFramePosition=-centroid,
     )
 
-    obj_id = p.createMultiBody(
+    obj_id = bullet_client.createMultiBody(
         baseMass=mesh.mass,
         basePosition=(1.0, 0, 0),
         baseOrientation=(0, 0, 0, 1),
@@ -144,22 +167,17 @@ def main():
         baseInertialFramePosition=np.array(mesh.center_mass - centroid),
     )
 
-    p.changeDynamics(obj_id, -1, lateralFriction=0.25)
-    p.setGravity(0, 0, -9.8)
+    bullet_client.changeDynamics(obj_id, -1, lateralFriction=0.25)
+    bullet_client.setGravity(0, 0, -9.8)
 
-    renderer = p.ER_BULLET_HARDWARE_OPENGL   # or p.ER_TINY_RENDERER
+    renderer = bullet_client.ER_BULLET_HARDWARE_OPENGL   # or pybullet_client.ER_TINY_RENDERER
 
     first_iteration = True
 
     while True:
-        camera_link_state = p.getLinkState(
-            robot_body_unique_id,
-            linkIndex=camera_link_idx,
-            computeForwardKinematics=1,  # True
-            physicsClientId=pybullet_client
-        )
-        camera_link_position = camera_link_state[0]      # tuple of 3 floats, same values as index [4]
-        camera_link_orientation = camera_link_state[1]   # tuple of 4 floats, same values as index [5]
+        camera_link_state = robot_body_unique_id.get_link_state_by_name(camera_joint_frame)
+        camera_link_position = camera_link_state.world_link_frame_position
+        camera_link_orientation = camera_link_state.world_link_frame_orientation
 
         camera_config["position"] = list(camera_link_position)
         camera_config["orientation"] = list(camera_link_orientation)
@@ -168,25 +186,22 @@ def main():
 
         if first_iteration:
             first_iteration = False
-            print(f"{camera_joint_frame} info:")
-            for elem in camera_link_state:
-                print(f"\t{elem}")
 
             print("Camera config:")
             print(camera_config)
 
-        width, height, rgb_img, depth_img, seg_img = p.getCameraImage(
+        width, height, rgb_img, depth_img, seg_img = bullet_client.getCameraImage(
             camera_config["image_size"][1],
             camera_config['image_size'][0], 
-            viewMatrix=get_view_matrix(deepcopy(camera_config)),
-            projectionMatrix=get_projection_matrix(deepcopy(camera_config)),
+            viewMatrix=get_view_matrix(bullet_client, deepcopy(camera_config)),
+            projectionMatrix=get_projection_matrix(bullet_client, deepcopy(camera_config)),
             shadow=0,
-            flags=p.ER_SEGMENTATION_MASK_OBJECT_AND_LINKINDEX,
+            flags=bullet_client.ER_SEGMENTATION_MASK_OBJECT_AND_LINKINDEX,
             renderer=renderer
             )
 
         time.sleep(0.01)
-        p.stepSimulation()
+        bullet_client.stepSimulation()
 
 if __name__ == "__main__":
     main()
